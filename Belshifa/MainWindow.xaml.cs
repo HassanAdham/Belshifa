f2550b5f4b01d25f77b17216486b668b16022151
Hassan Adham <32961869+Hassan1Adham@users.noreply.github.com>
2019-04-10 16:36:30 +0200

    cmmt

--- Belshifa/MainWindow.xaml.cs
+++ Belshifa/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace Belshifa
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
